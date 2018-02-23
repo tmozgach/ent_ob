606772ca01bea14e692db6fb9f4f053fb9d63b47
tmozgach <tmozgach@sfu.ca>
2018-02-22 16:03:30 -0800

    Thread (title, post and comments) per ROW

--- jsonTOcsvTopicModel.py
+++ jsonTOcsvTopicModel.py
@@ -1,5 +1,5 @@
 # Python 3
-# The following script takes the text of posts and comments from Reddit for Topic Modeling analysis
+# The following script takes the text of posts and title from Reddit for Topic Modeling analysis
 # Run inside the folder that contains your JSON files.
 
 import json
@@ -13,18 +13,24 @@ for filename in glob.glob('*.json'):
         for line in f:
 
             # The title is inside "title" field
-            title = re.findall(r'\"title\": \"(.+?)\",',line)
+            title = re.findall(r'\"title\": \"(.*?)\",',line)
             # The main post is inside "selftext" field
-            post = re.findall(r'\"selftext\": \"(.+?)\",',line) 
+            post = re.findall(r'\"selftext\": \"(.*?)\",',line) 
             # The comment is inside "body" field
-            comments = re.findall(r'\"body\": \"(.+?)\",',line)
+            comments = re.findall(r'\"body\": \"(.*?)\",',line)
+           
+            text = ' '.join([str(e) for e in title])
+            text = text + ' '
+            text = text + ' '.join(str(e) for e in post)
+            text = text + ' '
+            text = text + ' '.join(str(e) for e in comments) 
 
             # Write to CVS file
             with open("output.csv", "a+", newline='') as output:
                 writer = csv.writer(output)
-                writer.writerow(title)
-                writer.writerow(post)
-                for comment in comments:
-                    if comment != "[deleted]":
-                        writer.writerow([comment])
+                writer.writerow([text])
+                #writer.writerow(post)
+                #for comment in comments:
+                #    if comment != "[deleted]":
+                #       writer.writerow([comment])
 
